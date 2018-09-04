83a40fba3f009affd0ba474ed2e2bcca3657bc46
xukun <xukun@crxin.com>
2018-09-04 16:03:51 +0800

    添加
    添加.netcore项目

---
+++ Solution1/Crx.Entity/Entity/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crx.Domain.Entity
+{
+    public class User
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+    }
+}
